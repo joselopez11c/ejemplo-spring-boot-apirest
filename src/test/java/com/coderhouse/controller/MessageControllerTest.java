53573156937c6ed9b968572ca6b486bd6babb203
Jose Ignacio Lopez Cure <joselopez11c@gmail.com>
2022-01-18 23:23:37 -0500

    add test unit

--- src/test/java/com/coderhouse/controller/MessageControllerTest.java
+++ src/test/java/com/coderhouse/controller/MessageControllerTest.java
@@ -2,7 +2,6 @@ package com.coderhouse.controller;
 
 import com.coderhouse.model.Message;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.util.Assert;
 
 import java.util.List;
